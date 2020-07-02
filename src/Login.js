b8dc0845febc92818ce32a11318be338149290e7
shashis <shashis@chimeratechnologies.com>
2020-07-02 14:16:51 +0530

    initial login and register

---
+++ src/Login.js
@@ -0,0 +1,61 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { setUserSession } from './Utils/Common';
+
+function Login(props) {
+  const [loading, setLoading] = useState(false);
+  const email = useFormInput('');
+  const password = useFormInput('');
+  const [error, setError] = useState(null);
+
+  // handle button click of login form
+  const handleLogin = () => {
+    const token = Buffer.from(`${email.value}:${password.value}`, 'utf8').toString('base64')
+    console.log(email.value);
+    console.log(password.value);
+    setError(null);
+    setLoading(true);
+    axios.post('http://127.0.0.1:8080/auth/login', { email: email.value, password: password.value,
+      headers: {
+        'Authorization': `Basic ${token}`
+      }}).then(response => {
+      setLoading(false);
+      setUserSession(response.data.token, response.data.user);
+      props.history.push('/dashboard');
+    }).catch(error => {
+      setLoading(false);
+      if (error.response.status === 401) setError(error.response.data.message);
+      else setError("Something went wrong. Please try again later.");
+    });
+  }
+
+  return (
+    <div>
+      Login<br /><br />
+      <div>
+        Username<br />
+        <input type="email" {...email} autoComplete="new-password" />
+      </div>
+      <div style={{ marginTop: 10 }}>
+        Password<br />
+        <input type="password" {...password} autoComplete="new-password" />
+      </div>
+      {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+      <input type="button" value={loading ? 'Loading...' : 'Login'} onClick={handleLogin} disabled={loading} /><br />
+    </div>
+  );
+}
+
+const useFormInput = initialValue => {
+  const [value, setValue] = useState(initialValue);
+
+  const handleChange = e => {
+    setValue(e.target.value);
+  }
+  return {
+    value,
+    onChange: handleChange
+  }
+}
+
+export default Login;
