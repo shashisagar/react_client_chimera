3010250f7c461b879c2862510d2beda0b55e64e8
shashis <shashis@chimeratechnologies.com>
2020-10-04 21:27:32 +0530

    set port 8080

--- src/components/ChatWindows.js
+++ src/components/ChatWindows.js
@@ -26,7 +26,7 @@ class ChatWindows extends React.Component {
     }
     
     initSocketConnection() {
-        this.socket = socketIOClient.connect(`http://backendnodechimera.azurewebsites.net`);
+        this.socket = socketIOClient.connect(`http://backendnodechimera.azurewebsites.net:8080`);
     }
 
     setupSocketListeners() {
