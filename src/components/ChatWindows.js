09be7d5b75ddec93b27f6c8ac6cee809c0076531
shashis <shashis@chimeratechnologies.com>
2020-10-04 17:32:30 +0530

    added socket url

--- src/components/ChatWindows.js
+++ src/components/ChatWindows.js
@@ -26,7 +26,7 @@ class ChatWindows extends React.Component {
     }
     
     initSocketConnection() {
-        this.socket = socketIOClient.connect(`${process.env.REACT_APP_URL}`);
+        this.socket = socketIOClient.connect(`http://localhost:8080`);
     }
 
     setupSocketListeners() {
